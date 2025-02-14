e29aef1b7f8325892499be925deace5098ec8c7c
pinterbence <73128085+nerdice@users.noreply.github.com>
2025-02-14 08:37:58 +0100

    remelem mukodni fognak a seederek:(

--- database/seeders/UserBookSeeder.php
+++ database/seeders/UserBookSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Book;
+use Illuminate\Support\Facades\DB;
 
 class UserBookSeeder extends Seeder
 {
@@ -12,6 +15,35 @@ class UserBookSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = User::all();
+        $books = Book::all();
+
+        foreach ($users as $user) {
+
+            DB::table('user_book')->insert([
+            
+            'user_id' => $user->id,
+            
+            'book_id' => $books->random()->id,
+            
+            ]);
+            
+            DB::table('user_book')->insert([
+            
+            'user_id' => $user->id,
+            
+            'book_id' => $books->random()->id,
+            
+            ]);
+            
+            DB::table('user_book')->insert([
+            
+            'user_id' => $user->id,
+            
+            'book_id' => $books->random()->id,
+            
+            ]);
+            
+            }
     }
 }
